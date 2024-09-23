8a303c6d6285c4b98064f63b4b5e9ee358e59ff0
Глеб Дубинин <gleb1395@gmail.com>
2024-09-23 12:21:38 +0300

    Added diferent setings

--- src/config/settings/dev.py
+++ src/config/settings/dev.py
@@ -1,16 +1,16 @@
 from config.settings.base import *  # NOQA
 
-SECRET_KEY = 'django-insecure-_0#chxkmcfc@!myb)8*je4jbm5_bbs50flw29c-79ir(l*9v=t'
+SECRET_KEY = "django-insecure-_0#chxkmcfc@!myb)8*je4jbm5_bbs50flw29c-79ir(l*9v=t"
 
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",  # NOQA: F405
     }
 }
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
